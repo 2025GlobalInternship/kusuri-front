0a4c3fadca202d350f55a183e3e814e6c9c02d5a
연아윤 <w2306@e-mirim.hs.kr>
2025-02-13 10:46:05 +0900

    [feat] first페이지 추가

---
+++ src/components/search-layout.tsx
@@ -0,0 +1,5 @@
+export default function SearchLayout() {
+    return (
+        <>search</>
+    )
+}
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,10 @@
+import SearchLayout from "@/components/search-layout" //불러오기
+
 export default function Page() {
     return (
-        <>main</>
+        <>
+            main
+            <SearchLayout/>
+        </>
     )
 }
