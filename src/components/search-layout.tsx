ec6501b6818daff941cfac3a49fadc3650210343
soomin <s2305@e-mirim.hs.kr>
2025-02-14 01:13:45 +0900

    [feat] home 추가

--- src/components/search-layout.tsx
+++ src/components/search-layout.tsx
@@ -4,9 +4,9 @@ import searchIcon from "../../public/images/searchIcon.png";
 
 export default function SearchLayout() {
     return (
-        <div className={style.container}>
+        <form className={style.container}>
             <input id={style.searchTxt} type="text" />
             <Image id={style.searchIcon} src={searchIcon} alt="검색" />
-        </div>
+        </form>
     )
 }
