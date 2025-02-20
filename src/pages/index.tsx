c76190d775153e65b0b4d71b73dd5f7b475e602b
soomin <s2305@e-mirim.hs.kr>
2025-02-20 09:11:29 +0900

    [feat] 화면 전환 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Logo from "../../public/images/Frame 5700.png";
+import style from "./index.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return (
-    <>
-      
-    </>
-  );
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace('/login');
+    }, 2000);
+  });
+
+    return (
+        <div className={style.logo}>
+            <Image src={Logo} alt="로고"/>
+        </div>
+    )
 }
