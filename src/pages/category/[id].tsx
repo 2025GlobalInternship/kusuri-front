074b26bd5fb8fc2c63dc3578f213c2246e2390bd
soomin <s2305@e-mirim.hs.kr>
2025-05-21 03:19:49 +0900

    [fix] ssr을 이용한 약 정보 약품 페이지 구현

--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -1,48 +1,50 @@
 import HeaderLayout from "@/components/header-layout";
-import { useRouter } from "next/router";
 import style from "./[id].module.css";
 import MedicineLayout from "@/components/medicine-layout";
-import { useState } from "react";
+import { GetServerSideProps } from "next";
 
-export default function Page() {
-    const router = useRouter();
-
-    const {id} = router.query;
-    let [data, setData] = useState(
-        [
-            {
-                name: "약1",
-                tag: "감기",
-                detail: "어쩌구저쩌구"
-            },
-            {
-                name: "약2",
-                tag: "멀미미",
-                detail: "어쩌구저쩌구"
-            },
-            {
-                name: "약3",
-                tag: "알레르기",
-                detail: "어쩌구저쩌구"
-            }
-        ]
-    );
-
+export default function Page({ id, data }: { id: string; data: any[] }) {
     return (
-        <div key={"cate"+id}>
+        <div key={"cate" + id}>
             <HeaderLayout>{id}</HeaderLayout>
 
             <div className={style.mediCon}>
-                {
-                    data.map((a) => {
-                        return (
-                            <div key={a.name}>
-                                <MedicineLayout data={a} />
-                            </div>
-                        )
-                    })
-                }
+                {data.map((a) => (
+                    <div key={a.name}>
+                        <MedicineLayout data={a} />
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { id } = context.params as { id: string };
+
+    // 추후 fetch로 대체 예정
+    const data = [
+        {
+            name: "약1",
+            tag: "감기",
+            detail: "어쩌구저쩌구"
+        },
+        {
+            name: "약2",
+            tag: "멀미미",
+            detail: "어쩌구저쩌구"
+        },
+        {
+            name: "약3",
+            tag: "알레르기",
+            detail: "어쩌구저쩌구"
+        }
+    ];
+
+    return {
+        props: {
+            id,
+            data,
+        },
+    };
+};
