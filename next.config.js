5be6397dd4452b6ca7287e60ece55d73b23fbc5a
soomin <s2305@e-mirim.hs.kr>
2025-06-08 02:41:13 +0900

    [fix] 오타 수정

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost/kusuri-back/:path*', // ✔️ 정확한 주소
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
