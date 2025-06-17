81626c9dcfeaa8ff90ea952965e89bae3ed652dd
soomin <s2305@e-mirim.hs.kr>
2025-06-17 14:24:20 +0900

    [fix] 선언 후 사용하지 않은 배열 제거

--- src/pages/pharmacySearch-result/[id].tsx
+++ src/pages/pharmacySearch-result/[id].tsx
@@ -14,35 +14,12 @@ export default function Page() {
         router.push('/pharmacy');
     }
 
-    const data = [
-        {
-            name: "1",
-            length: 100
-        },
-        {
-            name: "2",
-            length: 200
-        },
-        {
-            name: "3",
-            length: 300
-        },
-        {
-            name: "4",
-            length: 400
-        },
-        {
-            name: "5",
-            length: 500
-        },
-    ];
-
     return (
         <div>
             <div id={style.headerCon}>
                     <Image src={chevronLeft} alt="" onClick={BackBtnClick} />
                     <div id={style.searchCon}>
-                        <PharmacySearchLayout text={id}>원하는 약국을 검색해주세요</PharmacySearchLayout>
+                        <PharmacySearchLayout text={id as string}>원하는 약국을 검색해주세요</PharmacySearchLayout>
                     </div>
             </div>
             <PharmacyList filterText={id as string} />
